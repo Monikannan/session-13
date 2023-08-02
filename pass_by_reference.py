df3122ac16ed68a9fcae183d41fc7a45b991bdb6
Monikannan <138356567+Monikannan@users.noreply.github.com>
2023-08-02 23:44:08 +0530

    Meet-up Assignment
    python

---
+++ pass_by_reference.py
@@ -0,0 +1,7 @@
+def test(student):
+    new = {'Darling':20,'Sweetie':21}
+    student.update(new)
+    return
+student = {'Anna':30,'Kani':20}
+test(student)
+print(student)
---
+++ pass_by_value.py
@@ -0,0 +1,7 @@
+def add(list):
+    list = ["Villupuram", "Ginux", "Linux","User","Group"]
+    return list
+my_list = ["Python"]
+ 
+print(add(list))
+print(my_list)
